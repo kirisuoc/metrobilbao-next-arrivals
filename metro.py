a0179163aec3d307580587b503a6b3ef7d3cdb5a
E10Y <e10ydev@protonmail.com>
2024-12-11 20:57:38 +0100

    New features in settings

--- metro.py
+++ metro.py
@@ -38,6 +38,11 @@ def load_data_default():
             origin, dest = set_station()
             set_data_default(origin, dest)
         return origin, dest
+    
+def reset_data_default():
+    set_data_default(None, None)
+
+
 
 def fetch_data(origin, destination):
     clear_terminal()
@@ -62,16 +67,16 @@ def fetch_data(origin, destination):
 
     console.print(table)
     console.input("\n[bold blue]Press ENTER to continue...[/bold blue]")
-    main()
+    show_menu()
 
 def set_station():
     clear_terminal()
     stations = load_data()
 
-	# Calcular el número máximo de estaciones por columna
+    # Calcular el número máximo de estaciones por columna
     half = (len(stations) + 1) // 2
 
-	# Dividir las estaciones en dos columnas
+    # Dividir las estaciones en dos columnas
     column1 = stations[:half]
     column2 = stations[half:]
 
@@ -80,8 +85,8 @@ def set_station():
 
     # Imprimir las estaciones en dos columnas
     for i in range(half):
-        col1 = f"[bold blue]{i + 1}[/bold blue]. {column1[i]['name']}" if i < len(column1) else ""
-        col2 = f"[bold blue]{i + 1 + half}[/bold blue]. {column2[i]['name']}" if i < len(column2) else ""
+        col1 = f"[bold blue]{i + 1:<3}[/bold blue] {column1[i]['name']:<30}" if i < len(column1) else ""
+        col2 = f"[bold blue]{i + 1 + half:<3}[/bold blue] {column2[i]['name']:<30}" if i < len(column2) else ""
         console.print(f"{col1:<45}{col2}")
 
     origin_index = IntPrompt.ask("[bold yellow]Enter the number of the origin station:[/bold yellow]") - 1
@@ -91,17 +96,68 @@ def set_station():
     console.print(f"[bold green]Selected origin station:[/bold green] {origin['name']}")
 
     console.print(Panel("[bold green]Select destination station:[/bold green]"))
-    # Imprimir las estaciones en dos columnas
+    
     for i in range(half):
-        col1 = f"[bold blue]{i + 1}[/bold blue]. {column1[i]['name']}" if i < len(column1) else ""
-        col2 = f"[bold blue]{i + 1 + half}[/bold blue]. {column2[i]['name']}" if i < len(column2) else ""
+        col1 = ""
+        col2 = ""
+
+        # Columna 1: destacar origen si coincide
+        if i < len(column1):
+            if column1[i] == origin:
+                col1 = f"[bold red]{i + 1:<3}[/bold red] [bold red]{column1[i]['name']:<30}[/bold red]"
+            else:
+                col1 = f"[bold blue]{i + 1:<3}[/bold blue] {column1[i]['name']:<30}"
+
+        if i < len(column2):
+            if column2[i] == origin:
+                col2 = f"[bold red]{i + 1 + half:<3}[/bold red] [bold red]{column2[i]['name']:<30}[/bold red]"
+            else:
+                col2 = f"[bold blue]{i + 1 + half:<3}[/bold blue] {column2[i]['name']:<30}"
+
         console.print(f"{col1:<45}{col2}")
 
     destination_index = IntPrompt.ask("[bold yellow]Enter the number of the destination station:[/bold yellow]") - 1
     destination = stations[destination_index]
 
     console.print(f"[bold green]Selected destination station:[/bold green] {destination['name']}")
+    if destination == origin:
+        console.print("[bold red]\nError: Choose different origin and destination\n[/bold red]")
+        console.input("\n[bold blue]Press ENTER to return...[/bold blue]")
+        return set_station()
     return origin, destination
+
+
+def settings():
+    clear_terminal()
+    with open("stations.json", "r") as file:
+        data = json.load(file)
+        start = data["default_start"]
+
+    console.print(Panel("[bold green]Settings:[/bold green]"))
+    console.print(Panel(f"[bold yellow]1 - Reset Default Trip.\n2 - Launch with Default Trip: [bold green]{start}[/bold green]\n3 - Exit.[/bold yellow]"))
+    option = IntPrompt.ask("[bold cyan]Select an option:[/bold cyan]")
+
+    if option == 1:
+       reset_data_default()
+       console.input("\n[bold blue]The default trip has been deleted.[/bold blue]")
+       console.input("\n[bold blue]Press ENTER to return to the menu...[/bold blue]")
+    elif option == 2:
+        with open("stations.json", "r") as file:
+            data = json.load(file)
+            if data["default_start"] == True:
+                data["default_start"] = False
+            else:
+                data["default_start"] = True
+        with open("stations.json", "w") as file:
+            json.dump(data, file, indent=4)
+        settings()
+
+    elif option == 3:
+        show_menu()
+    else:
+        console.print("[bold red]\nError: Invalid option!!!\n[/bold red]")
+        console.input("\n[bold blue]Press ENTER to return to the menu...[/bold blue]")
+        show_menu()
 
 def travel_now():
     origin, destination = set_station()
@@ -114,11 +170,11 @@ def default_trip():
 def show_logo():
     clear_terminal()
     logo = """
- __  __      _                 ____  _ _ _
-|  \/  | ___| |_ _ __ ___     | __ )(_) | |__   __ _  ___
-| |\/| |/ _ \ __| '__/ _ \ ___|  _ \| | | '_ \ / _` |/ _ \
+ __  __      _                 ____  _ _ _                 
+|  \/  | ___| |_ _ __ ___     | __ )(_) | |__   __ _  ___  
+| |\/| |/ _ \ __| '__/ _ \ ___|  _ \| | | '_ \ / _` |/ _ \ 
 | |  | |  __/ |_| | | (_) |___| |_) | | | |_) | (_| | (_) |
-|_|  |_|\___|\__|_|  \___/    |____/|_|_|_.__/ \__,_|\___/
+|_|  |_|\___|\__|_|  \___/    |____/|_|_|_.__/ \__,_|\___/ 
     """
     console.print(Align.center(Panel(logo, title="Metro Bilbao", style="bold magenta")))
 
@@ -133,8 +189,7 @@ def show_menu():
     elif option == 2:
         default_trip()
     elif option == 3:
-        console.print("[bold red]TO DO.[/bold red]")
-        console.input("\n[bold blue]Press ENTER to return to the menu...[/bold blue]")
+        settings()
     elif option == 4:
         console.print("[bold green]Goodbye![/bold green]")
         exit()
@@ -144,7 +199,13 @@ def show_menu():
     main()
 
 def main():
-    show_menu()
+    with open("stations.json", "r") as file:
+        data = json.load(file)
+    
+    if data["default_start"]:
+        default_trip()
+    else:
+        show_menu()
 
 if __name__ == "__main__":
     main()
